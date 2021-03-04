3cd23f8ffe407d0d50ace1980cc227a8bc89eb88
mariselvan98 <61959655+mariselvan98@users.noreply.github.com>
2021-03-04 12:14:08 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- font awsome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css"
+        integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+
+    <!-- material font icon -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="index.css">
+    <title>Construction</title>
+
+    
+</head>
+
+
+
+
+
+<body>
+
+    <div class="container-fluid">
+        <div class="banner-1 ">
+            <img class="imgslide" src="../docs/images/cons7.jpg" alt="">
+            <div class="text-box text-box1">
+                <span class="number">01</span>
+                <br>
+                <h1>Experts In The Local Market...</h1>
+                <span class="line"></span>
+            </div>
+        </div>
+
+        <!-- logo and construction name -->
+        <div class="logo">
+            <img src="../docs/images/logo.png" alt="">
+            <h3>Construction Name</h3>
+        </div>
+
+        <div class="banner-2">
+            <img class="imgslide" src="../docs/images/cons1.jpg" alt="">
+            <div class="text-box text-box2">
+                <span class="number">02</span>
+                <br>
+                <h1>Calculate Your Plan...</h1>
+                <span class="line"></span>
+            </div>
+        </div>
+
+        <div class="banner-3">
+            <img class="imgslide" src="../docs/images/cons2.jpg" alt="">
+            <div class="text-box text-box3">
+                <span class="number">03</span>
+                <br>
+                <h1>Design Your Plan...</h1>
+                <span class="line"></span>
+            </div>
+        </div>
+
+        <div class="banner-4">
+            <img class="imgslide" src="../docs/images/cons8.jpg" alt="">
+            <div class="text-box text-box4">
+                <span class="number">04</span>
+                <br>
+                <h1>Build Your Plan...</h1>
+                <span class="line"></span>
+            </div>
+        </div>
+
+        <div class="banner-5">
+            <img class="imgslide" src="../docs/images/cons5.jpg" alt="">
+            <div class="text-box text-box5">
+                <span class="number">05</span>
+                <br>
+                <h1>We Make Your Home An <strong style="color: white;"> Island...</strong></h1>
+                <span class="line"></span>
+            </div>
+        </div>
+
+        <a href="#"><button class="btn btn-outline-danger">Register</button></a>
+
+
+        <div class="row">
+            <div class="col-md-4" id="frame1">
+                <a href="#">
+                    <img src="../docs/images/frame4.png" alt="">
+                    <span><i class="material-icons">av_timer</i>Range</span>
+                </a>
+            </div>
+
+            <div class="col-md-4" id="frame2">
+                <a href="#">
+                    <img src="../docs/images/frame4.png" alt="">
+                    <span><i class="material-icons">settings_input_svideo</i>Lumpsum</span>
+                </a>
+
+            </div>
+
+            <div class="col-md-4" id="frame3">
+                <a href="#">
+                    <img src="../docs/images/frame4.png" alt="">
+                    <span><i class="material-icons">pending_actions</i>Report</span>
+                </a>
+
+            </div>
+
+        </div>
+    </div>
+    
+
+</html>
